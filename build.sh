ca9b85bda641587abe8a59a6e05a84cb17b8a42d
NIRUBAN <nrbn@live.no>
2019-05-13 10:34:26 +0200

    removed file

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#cd ~/Downloads/prosjekter/CrewCall/crewcall
-ng build --prod || exit 9
-#sed -i 's#<base href="/">#<base href="/public/userfront/">#' dist/crewcall/index.html
-rsync -az --delete -e ssh dist/ nivel@leke.crewcall.no:CrewcallFront/
-
-#rsync -az --delete -e ssh ./ nivel@leke.crewcall.no:CrewcallFront/
-cp .htaccess ./dist/crewcall
